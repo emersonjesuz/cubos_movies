c9c9acc89bb96001cf31d8735c07282e5c3f275a
emersonjesuz <josiemerson2013@gmail.com>
2025-10-23 20:12:35 -0300

    feat: add method login in AuthUseCase and add LoginInput dto

--- server/src/auth/AuthUsecase.ts
+++ server/src/auth/AuthUsecase.ts
@@ -1,11 +1,18 @@
 import { PasswordEncoder } from "../shared/security/PasswordEncoder";
+import { TokenService } from "../shared/security/TokenService";
 import { UserEntity } from "../users/UserEntity";
 import { UserRepository } from "../users/UserRepository";
+import { LoginInput } from "./dtos/LoginInput";
 import { RegisterInput } from "./dtos/RegisterInput";
+import { EmailOrPasswordIncorrectException } from "./exceptions/EmailOrPasswordIncorrectException";
 import { UserAlreadyExistsException } from "./exceptions/UserAlreadyExistsException";
 
 export class AuthUseCase {
-  constructor(private readonly userRepository: UserRepository, private readonly passwordEncoder: PasswordEncoder) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly passwordEncoder: PasswordEncoder,
+    private readonly tokenService: TokenService
+  ) {}
 
   public async register(input: RegisterInput): Promise<void> {
     const hasUser = await this.userRepository.findByEmail(input.email);
@@ -13,5 +20,12 @@ export class AuthUseCase {
     const passwordHash = await this.passwordEncoder.encode(input.password);
     const newUser = new UserEntity(input.name, input.email, passwordHash);
     await this.userRepository.save(newUser);
+  }
+  public async login(input: LoginInput): Promise<string> {
+    const user = await this.userRepository.findByEmail(input.email);
+    if (!user) throw new EmailOrPasswordIncorrectException();
+    const passwordIsMatches = await this.passwordEncoder.matches(input.password, user.getPassword());
+    if (passwordIsMatches) throw new EmailOrPasswordIncorrectException();
+    return this.tokenService.generated({ email: user.getEmail() });
   }
 }
