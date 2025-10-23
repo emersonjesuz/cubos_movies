b720f08d8658de282ff69e0560389fb5efbdd48a
emersonjesuz <josiemerson2013@gmail.com>
2025-10-23 19:20:54 -0300

    feat: add zod schema and authSchema

---
+++ server/src/auth/authSchema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const registerUserSchema = z.object({
+  name: z
+    .string({ error: "MESSAGE: The field 'name' is obligatory.  CODE: NAME_IS_OBLIGATORY" })
+    .nonempty({ error: "MESSAGE: The field 'name' is obligatory.  CODE: NAME_IS_OBLIGATORY" }),
+  email: z
+    .string({ error: "MESSAGE: The field 'email' is obligatory.  CODE: EMAIL_IS_OBLIGATORY" })
+    .email({ error: "MESSAGE: The field 'email' is invalid.  CODE: EMAIL_IS_INVALID" }),
+  password: z
+    .string({ error: "MESSAGE: The field 'password' is obligatory.  CODE: PASSWORD_IS_OBLIGATORY" })
+    .min(6, { error: "MESSAGE: The field 'password' must be at least 6 characters long.  CODE: PASSWORD_IS_INVALID" }),
+});
