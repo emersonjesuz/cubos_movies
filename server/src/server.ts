c7d40ab5de904a358b4117fd10e4d15bfbd9354c
emersonjesuz <josiemerson2013@gmail.com>
2025-10-23 19:23:58 -0300

    feat: add router register and middleware error

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import cors from "cors";
+import { AuthController } from "./auth/AuthController";
+import { ErrorHandler } from "./shared/middleware/ErrorHandler";
 
 const app = express();
+const authController = new AuthController();
+const handlerError = new ErrorHandler();
 
 app.use(cors());
 app.use(express.json());
 
+app.post("/auth/register", (req, res) => authController.register(req, res));
+
+app.use(handlerError.process);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
