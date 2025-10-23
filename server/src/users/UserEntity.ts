7aebd652a644955eb6745bc0ae86020a0b5a5f3b
emersonjesuz <josiemerson2013@gmail.com>
2025-10-23 17:43:40 -0300

    refactor: add getters and setters in UserEntity

--- server/src/users/UserEntity.ts
+++ server/src/users/UserEntity.ts
@@ -1,10 +1,10 @@
 export class UserEntity {
-  private id: string;
+  private id?: string;
   private name: string;
   private email: string;
   private password: string;
 
-  constructor(id: string, name: string, email: string, password: string) {
+  constructor(name: string, email: string, password: string, id?: string) {
     this.id = id;
     this.name = name;
     this.email = email;
@@ -12,15 +12,23 @@ export class UserEntity {
   }
 
   public getId(): string {
-    return this.id;
+    return this.id!;
   }
 
   public getName(): string {
     return this.name;
   }
 
+  public setName(value: string) {
+    this.name = value;
+  }
+
   public getEmail(): string {
     return this.email;
+  }
+
+  public setEmail(value: string) {
+    this.email = value;
   }
 
   public getPassword(): string {
