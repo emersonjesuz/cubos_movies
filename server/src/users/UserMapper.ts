2738b7167b25dd5bab4b71844cdb3bfab1a64ed8
emersonjesuz <josiemerson2013@gmail.com>
2025-10-24 09:55:09 -0300

    fix: passing the right parameters in UserEntity

--- server/src/users/UserMapper.ts
+++ server/src/users/UserMapper.ts
@@ -3,6 +3,6 @@ import { UserEntity } from "./UserEntity";
 
 export class UserMapper {
   public static toUserEntity(userDb: Users): UserEntity {
-    return new UserEntity(userDb.id, userDb.name, userDb.email, userDb.password);
+    return new UserEntity(userDb.name, userDb.email, userDb.password, userDb.id);
   }
 }
