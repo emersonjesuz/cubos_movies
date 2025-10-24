9209e555ef2d6ccc7c783533d6c1fc95bf4a97fe
emersonjesuz <josiemerson2013@gmail.com>
2025-10-23 23:02:35 -0300

    fix: refactor import

--- server/src/users/UserMapper.ts
+++ server/src/users/UserMapper.ts
@@ -1,8 +1,8 @@
-import { User } from "@prisma/client";
+import { Users } from "@prisma/client";
 import { UserEntity } from "./UserEntity";
 
 export class UserMapper {
-  public static toUserEntity(userDb: User): UserEntity {
+  public static toUserEntity(userDb: Users): UserEntity {
     return new UserEntity(userDb.id, userDb.name, userDb.email, userDb.password);
   }
 }
