298a611147c42ccaa0fac981cb89c4b893ef6f04
emersonjesuz <josiemerson2013@gmail.com>
2025-10-24 16:03:01 -0300

    feat: add Rabbitmq in  docker-compose and Resend to send email

---
+++ server/src/shared/services/email/EmailService.ts
@@ -0,0 +1,29 @@
+import { Resend } from "resend";
+
+type EmailInput = {
+  email: string;
+  subject: string;
+  html: string;
+};
+
+export interface EmailService {
+  send(input: EmailInput): Promise<void>;
+}
+
+export class EmailResendService implements EmailService {
+  private readonly SECRET = process.env.EMAIL_SECRET;
+  private readonly FROM_EMAIL = process.env.FROM_EMAIL;
+  async send(input: EmailInput): Promise<void> {
+    const resend = new Resend(this.SECRET);
+    const { data, error } = await resend.emails.send({
+      from: this.FROM_EMAIL || "onboarding@resend.dev",
+      to: input.email,
+      subject: input.subject,
+      html: input.html,
+    });
+
+    if (error) {
+      throw new Error("Error send email.");
+    }
+  }
+}
