32bdaaa4062d204c8ae4df4c7382100d28470a37
emersonjesuz <josiemerson2013@gmail.com>
2025-10-25 01:09:39 -0300

    feat: add toMovieOutput method in MovieDbMapper

--- server/src/movies/mappers/MovieDbMapper.ts
+++ server/src/movies/mappers/MovieDbMapper.ts
@@ -1,5 +1,7 @@
 import { MovieWithUser } from "../../shared/interfaces/MovieWithUser";
 import { UserEntity } from "../../users/UserEntity";
+import { MovieOutput } from "../dtos/MovieOutput";
+import { MovieUserOutput } from "../dtos/MovieUserOutput";
 import { MovieEntity } from "../MovieEntity";
 
 export class MovieDbMapper {
@@ -28,6 +30,34 @@ export class MovieDbMapper {
       input.director,
       new UserEntity(user.name, user.email, "", user.id),
       input.id
+    );
+  }
+  public static toMovieOutput(input: MovieWithUser): MovieOutput {
+    const user = input.users!;
+    return new MovieOutput(
+      input.id,
+      input.title,
+      input.originalTitle,
+      input.synopsis,
+      input.description,
+      input.popularity,
+      input.release,
+      input.budget.toNumber(),
+      input.revenue.toNumber(),
+      input.votes,
+      input.duration,
+      input.language,
+      input.profit.toNumber(),
+      input.status,
+      input.urlCover,
+      input.urlBackground,
+      input.urlTrailer,
+      input.approvalRating,
+      input.CreatedAt,
+      input.genres,
+      input.ageRating,
+      input.director,
+      new MovieUserOutput(user.id, user.name)
     );
   }
 }
